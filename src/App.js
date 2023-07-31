5969f1e5a05837bdbff21e057420ee3a71ad90e3
LilianKanyua <kanyualilianm@gmail.com>
2023-07-31 16:33:36 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import GetMovies from './Components/GetMovies';
 import Navbar from './Components/Navbar/navbar';
-import GetMovieDetails from './Components/MovieDetails/details';
+
 
 function App() {
   return (
     <div>
        <Navbar/>
       <GetMovies/>
-      <GetMovieDetails/>
+     
      
     </div>
   );
--- src/Utils/utilities.js
+++ src/Utils/utilities.js
@@ -1,6 +1,7 @@
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 const ACCESS_TOKEN = process.env.REACT_APP_ACCESS_TOKEN;
 
+
 export const getMovies = async () =>{
     try{
         const response = await fetch (`${BASE_URL}/3/movie/popular`, {
@@ -37,20 +38,3 @@ export const searchMovies = async (query) => {
     }
   };
  
-export const getMovieDetails = async () =>{
-  try{
-      const response = await fetch (`${BASE_URL}/3/movie/popular`, {
-      method: 'GET',
-      headers:{
-          Authorization: `Bearer ${ACCESS_TOKEN}`,
-          'Content-Type': 'application/json'
-      }
-  })
-      const result = await response.json();
-      return result
-  }
- 
-  catch(error){
-      return error.message;
-  }
-}
